a6cbe70ef46b02e27271b75cf8892ff65ffafd17
mdkaifq <kaifqureshi360@gmail.com>
2025-06-29 21:50:40 +0530

    initial commit

---
+++ app/services/worker/celery.py
@@ -0,0 +1,14 @@
+from celery import Celery
+from app.core.config import settings
+
+TASK_PATH = 'app.services.worker.tasks'
+BROKER = settings.CELERY_BROKER_URL
+BACKEND = settings.CELERY_RESULT_BACKEND
+
+celery = Celery(
+    'app.main',                       # Specify the path to your main module
+    include=[TASK_PATH],     # Make sure to provide the correct path to your tasks
+    broker=BROKER,
+    backend=BACKEND,
+    broker_connection_retry_on_startup=True
+)
