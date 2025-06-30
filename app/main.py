efabbba69bd71dcf9fb2fa37209d482e99a386b3
mdkaifq <kaifqureshi360@gmail.com>
2025-06-30 14:29:23 +0530

    fixed exception handler

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ async def shutdown_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['localhost:8080'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
